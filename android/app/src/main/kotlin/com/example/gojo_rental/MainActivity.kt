ffc694f59b560abb7ab206a3b3dfb9c836220203
bettykassaw <betty01kassaw@gmail.com>
2022-08-30 09:16:40 +0300

    first signup

---
+++ android/app/src/main/kotlin/com/example/gojo_rental/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gojo_rental
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/screen/signup.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class signup extends StatefulWidget {
+  const signup({Key? key}) : super(key: key);
+
+  @override
+  State<signup> createState() => _signupState();
+}
+
+class _signupState extends State<signup> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
